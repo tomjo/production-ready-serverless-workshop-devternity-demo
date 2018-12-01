63fe4ec8ad03bf72193854f5e7e8541a675689c9
tomjo <Tom.Jorissen@cegeka.com>
2018-12-01 14:06:00 +0100

    add structured logging

--- functions/place-order.js
+++ functions/place-order.js
@@ -1,34 +1,36 @@
-const AWS        = require('aws-sdk')
-const kinesis    = new AWS.Kinesis()
-const chance     = require('chance').Chance()
-const streamName = process.env.order_events_stream
+
+const AWS        = require('aws-sdk');
+const kinesis    = new AWS.Kinesis();
+const chance     = require('chance').Chance();
+const streamName = process.env.order_events_stream;
+const Log = require('../lib/log');
 
 module.exports.handler = async (event, context) => {
-  const restaurantName = JSON.parse(event.body).restaurantName
+  const restaurantName = JSON.parse(event.body).restaurantName;
 
-  const orderId = chance.guid()
-  console.log(`placing order ID [${orderId}] to [${restaurantName}]`)
+  const orderId = chance.guid();
+  Log.debug(`placing order ID [${orderId}] to [${restaurantName}]`);
 
   const data = {
     orderId,
     restaurantName,
     eventType: 'order_placed'
-  }
+  };
 
   const req = {
     Data: JSON.stringify(data), // the SDK would base64 encode this for us
     PartitionKey: orderId,
     StreamName: streamName
-  }
+  };
 
-  await kinesis.putRecord(req).promise()
+  await kinesis.putRecord(req).promise();
 
-  console.log(`published 'order_placed' event into Kinesis`)
+  Log.debug(`published 'order_placed' event into Kinesis`);
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({ orderId })
-  }
+  };
 
   return response
-}
+};
