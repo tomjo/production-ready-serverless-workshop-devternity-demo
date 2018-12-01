220f0ceaa67f57d0b98b419e1741874574f147a6
tomjo <Tom.Jorissen@cegeka.com>
2018-12-01 15:51:11 +0100

    add debug log enabled passing

--- middleware/sample-logging.js
+++ middleware/sample-logging.js
@@ -1,4 +1,5 @@
 const Log = require('../lib/log');
+const CorrelationIds = require('../lib/correlation-ids');
 
 // config should be { sampleRate: double } where sampleRate is between 0.0-1.0
 module.exports = (config) => {
@@ -6,7 +7,14 @@ module.exports = (config) => {
     let rollback = undefined;
 
     const isDebugEnabled = () => {
-        return sampleRate && Math.random() <= sampleRate
+        const context = CorrelationIds.get();
+        if (context['debug-log-enabled'] === 'true') {
+            return true
+        } else if (context['debug-log-enabled'] === 'false') {
+            return false
+        } else {
+            return sampleRate && Math.random() <= sampleRate
+        }
     };
 
     return {
