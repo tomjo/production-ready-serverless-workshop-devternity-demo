7df751884b305756045d2ddbbd474d6225270737
tomjo <Tom.Jorissen@cegeka.com>
2018-12-01 11:25:06 +0100

    add notify-restaurant testcase

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -7,6 +7,7 @@ const init = async () => {
 
     process.env.restaurants_api   = "https://1tz6miu631.execute-api.eu-west-1.amazonaws.com/dev/restaurants";
     process.env.restaurants_table = "restaurants-dev-tomjo";
+    process.env.restaurants_notification_topic = "restaurants-dev-tomjo";
     process.env.AWS_REGION        = "eu-west-1";
 
     initialized = true
