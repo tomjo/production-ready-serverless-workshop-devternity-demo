871f88b4caba1a143f3ad396e8a7c2a12b8cd01e
tomjo <Tom.Jorissen@cegeka.com>
2018-12-01 11:14:36 +0100

    add get-index testcase

---
+++ tests/steps/init.js
@@ -0,0 +1,17 @@
+let initialized = false;
+
+const init = async () => {
+    if (initialized) {
+        return
+    }
+
+    process.env.restaurants_api   = "https://1tz6miu631.execute-api.eu-west-1.amazonaws.com/dev/restaurants";
+    process.env.restaurants_table = "restaurants-dev-tomjo";
+    process.env.AWS_REGION        = "eu-west-1";
+
+    initialized = true
+};
+
+module.exports = {
+    init
+};
